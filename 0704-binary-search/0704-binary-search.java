383e788ac346ca9f6eac5584d7cd8a46558256e9
JBSanjay <105296246+JBSanjay@users.noreply.github.com>
2023-01-15 19:28:47 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (96.26%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,24 +1,40 @@
 class Solution {
     public int binsearch(int []nums,int target,int low,int high)
     {
-        if(nums==null || high>nums.length)
+        if(nums==null || low>high)
         {
             return -1;
         }
-        if(low>high)
+        else
         {
-            return -1;
+             int mid=(low+high)/2;
+             if(nums[mid]==target)
+             {
+               return mid;
+             }
+            else if(nums[mid]>target)
+            {
+                return binsearch(nums,target,low,mid-1);
+            }
+            else
+            {
+                 return binsearch(nums,target,mid+1,high);
+            }
         }
-        int mid=(low+high)/2;
-        if(nums[mid]==target)
-        {
-            return mid;
-        }
-        if(nums[mid]>target)
-        {
-            return binsearch(nums,target,low,mid-1);
-        }
-        return binsearch(nums,target,mid+1,high);
+        // if(low>high)
+        // {
+        //     return -1;
+        // }
+        // int mid=(low+high)/2;
+        // if(nums[mid]==target)
+        // {
+        //     return mid;
+        // }
+        // if(nums[mid]>target)
+        // {
+        //     return binsearch(nums,target,low,mid-1);
+        // }
+        // return binsearch(nums,target,mid+1,high);
     }
     public int search(int[] nums, int target) {
         return binsearch(nums,target,0,nums.length-1);
